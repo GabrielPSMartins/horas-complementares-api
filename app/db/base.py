1517c81168de993d8aedcf1307ba319ab0a65fb0
GabrielPSMartins <martins80gabriel@gmail.com>
2026-07-08 19:34:33 -0300

    feat: adicionando a modelagem inicial de domínio

--- app/db/base.py
+++ app/db/base.py
@@ -3,3 +3,14 @@ from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
+
+
+from app.models import (  
+    ActivityAttachment,
+    ActivityRequest,
+    ActivityRequestHistory,
+    ActivityType,
+    Course,
+    Student,
+    User,
+)
---
+++ app/models/__init__.py
@@ -0,0 +1,19 @@
+from app.models.activity_attachment import ActivityAttachment
+from app.models.activity_request import ActivityRequest, ActivityRequestStatus
+from app.models.activity_request_history import ActivityRequestHistory
+from app.models.activity_type import ActivityType
+from app.models.course import Course
+from app.models.student import Student
+from app.models.user import User, UserRole
+
+__all__ = [
+    "ActivityAttachment",
+    "ActivityRequest",
+    "ActivityRequestStatus",
+    "ActivityRequestHistory",
+    "ActivityType",
+    "Course",
+    "Student",
+    "User",
+    "UserRole",
+]
