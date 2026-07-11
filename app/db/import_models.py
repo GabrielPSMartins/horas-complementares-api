1f5a6bac46e74345b294579065abd963b55bccd2
GabrielPSMartins <martins80gabriel@gmail.com>
2026-07-11 18:45:01 -0300

    feat: adiciona seed de dados iniciais de desenvolvimento

---
+++ app/db/import_models.py
@@ -0,0 +1,7 @@
+from app.models.activity_attachment import ActivityAttachment  
+from app.models.activity_request import ActivityRequest  
+from app.models.activity_request_history import ActivityRequestHistory  
+from app.models.activity_type import ActivityType
+from app.models.course import Course  
+from app.models.student import Student 
+from app.models.user import User  
