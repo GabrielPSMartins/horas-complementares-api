8a757ba1a4eec4f064cb7fa7c894d977a3a07814
GabrielPSMartins <martins80gabriel@gmail.com>
2026-07-19 18:31:21 -0300

    feat: adiciona rota para listagem de tipos de atividades"

--- config/settings.py
+++ config/settings.py
@@ -17,12 +17,14 @@ class Settings(BaseSettings):
     pgadmin_default_email: str
     pgadmin_default_password: str
 
-    SECRET_KEY: str
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+    jwt_refresh_token_expire_days: int
 
-    default_root_password: str | None = None
-    default_coordinator_password: str | None = None
+    default_root_password: str
+    default_coordinator_password: str
+
 
     model_config = SettingsConfigDict(
         env_file=".env",
